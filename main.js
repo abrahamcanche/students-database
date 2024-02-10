09e49cb55493a86afbe308095363f866dd01cd69
Abraham ACER <al056705@uacam.mx>
2024-02-09 19:47:04 -0600

    La función para asignar calificaciones fue creada

--- main.js
+++ main.js
@@ -52,12 +52,18 @@ console.log(DataBase.isEmpty());
 
 //Las materias que se asignarán de manera aleatória
 const materiasDeCoding = ["HTML", "CSS", "JavaScript"]
-const materiasDeEconomia = ["Cálculo", "Estadística", "Econometría", "Finanzas"]
+const materiasDeEconomia = ["Estadística", "Desarrollo Regional", "Finanzas"]
 const materiasDeMusica = ["Teoría musical", "Técnica de instrumento", "Ensamble"]
 const asignaturas = [materiasDeCoding, materiasDeEconomia, materiasDeMusica]
 
 // Generar un número aleatorio entre 0 y 2 (la longitud de la lista de opciones)
 const indiceAleatorio = Math.floor(Math.random() * asignaturas.length)
+
+//Las posibles calificaciones
+const posiblesCalificaciones = [5, 6, 7, 8, 9, 10]
+const indiceAleatorio2 =Math.floor(Math.random() * posiblesCalificaciones.length)
+const indiceAleatorio3 =Math.floor(Math.random() * posiblesCalificaciones.length)
+const indiceAleatorio4 =Math.floor(Math.random() * posiblesCalificaciones.length)
 
 //La clase para crear alumnos y añadirlos a la DataBase
 class Alumno {
@@ -72,10 +78,14 @@ class Alumno {
     //Función para asignarle sus materias de manera aleatoria
     subjects () {
         this.materias = asignaturas[indiceAleatorio];
-        } 
-    //Función para asignarle sus calificaciones
+        }
 
-    //Función para añadir a la pila
+    //Función para asignarle sus calificaciones
+    grades () {
+        this.calificaciones = [posiblesCalificaciones[indiceAleatorio2], posiblesCalificaciones[indiceAleatorio3], posiblesCalificaciones[indiceAleatorio4]];
+    }
+
+        //Función para añadir a la pila
     joinToDataBase () {
         DataBase.push(this);
     }
@@ -84,22 +94,20 @@ class Alumno {
 //Creé los primeros alumnos
 let alumno1 = new Alumno ("Ángel", "Canché", 22)
 let alumno2 = new Alumno ("Abraham", "Cajún", 22)
-
-//Mando a llamar a la función que añadé a los alumnos a la base de datos
-alumno1.joinToDataBase();
-console.log(DataBase.peek());
-
-alumno2.joinToDataBase();
-console.log(DataBase.peek());
-
 let alumno3 = new Alumno ("Joselito", "Wisconsin", 19)
-alumno3.joinToDataBase();
-console.log(DataBase.peek());
-
-DataBase.clear();
-console.log(DataBase.isEmpty);
 
 //Testeando
 alumno1.subjects();
+alumno1.grades();
 alumno1.joinToDataBase();
+console.log(DataBase.peek());
+
+alumno2.subjects();
+alumno2.grades();
+alumno2.joinToDataBase();
+console.log(DataBase.peek());
+
+alumno3.subjects();
+alumno3.grades();
+alumno3.joinToDataBase();
 console.log(DataBase.peek());
